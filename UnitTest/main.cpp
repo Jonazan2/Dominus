0de24723756318dc6d2fad0cd291cd794986b4d8
achambi <alvaro.chambi.campos@gmail.com>
2016-02-28 18:54:15 +0100

    Added gtest and gmock framework to the project

---
+++ UnitTest/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  UnitTest
+//
+//  Created by Alvaro Chambi Campos on 28/2/16.
+//  Copyright © 2016 frikazos. All rights reserved.
+//
+
+#include "gtest/gtest.h"
+#include <gmock/gmock.h>
+
+TEST (Calculator, Addition)
+{
+    // equal to 20
+    EXPECT_EQ(20, 20);
+    
+}
+
+int main(int argc, char * argv[])
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    //::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
